a1e66d39e3edadecc4a689d550e2331959652e4a
Arthur Van Betsbrugge <avbetsbr@gmail.com>
2020-01-19 09:32:42 -0500

    Changed colors

--- JackBike/app/src/main/java/com/squad/jackbike/ItenerarySelectorActivity.java
+++ JackBike/app/src/main/java/com/squad/jackbike/ItenerarySelectorActivity.java
@@ -10,20 +10,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ItenerarySelectorActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button[] = new Button[10];
     @NonNull
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate((savedInstanceState));
         setContentView(R.layout.activity_itinerary_selector);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMap();
-            }
-        });
+        button[0] = (Button) findViewById(R.id.button);
+        button[1] = (Button) findViewById(R.id.destination1);
+        button[2] = (Button) findViewById(R.id.destination2);
+        button[3] = (Button) findViewById(R.id.destination3);
+        button[4] = (Button) findViewById(R.id.destination4);
+        button[5] = (Button) findViewById(R.id.destination5);
+        button[6] = (Button) findViewById(R.id.destination6);
+        button[7] = (Button) findViewById(R.id.destination7);
+        button[8] = (Button) findViewById(R.id.destination8);
+        button[9] = (Button) findViewById(R.id.destination9);
+
+
+        for(int i = 0; i < 10; i++) {
+            button[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openMap();
+                }
+            });
+        }
     }
     public void openMap(){
         Intent intent = new Intent(this, MapsActivity.class);
