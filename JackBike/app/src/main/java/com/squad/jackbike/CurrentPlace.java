a785b8c37bea60e36d71da06c3af649c0dd81d4e
Katherine Zamudio-Turcotte <kathy_zt@yahoo.ca>
2020-01-18 19:20:27 -0500

    getCurrentLocation method completed
    a new class named CurrentPlace was created with the method getCurrentLocation, it is being called and used in MapsActivity

---
+++ JackBike/app/src/main/java/com/squad/jackbike/CurrentPlace.java
@@ -0,0 +1,46 @@
+package com.squad.jackbike;
+
+import android.location.Location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
+public class CurrentPlace extends AppCompatActivity {
+
+    // Default location is Montreal if not found
+    private LatLng currentLocation = new LatLng(45, -73);
+
+    // Instance to use Google Play Services' location API
+    private FusedLocationProviderClient locationProvider;
+
+    public CurrentPlace() {
+        // initiate location provider (get the client)
+        locationProvider = LocationServices.getFusedLocationProviderClient(this);
+
+        // retreive last known location
+        locationProvider.getLastLocation()
+                // react only on success
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // the location found is a Location object
+                        if (location != null) {
+                            // updating "currentLocation" with the location found
+                            currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        }
+                        // The Location object can be null in rare cases. If that happens, currentLocation will not be updated.
+                    }
+                });
+    }
+
+    public LatLng getCurrentLocation() {
+        return currentLocation;
+    }
+
+
+}
--- JackBike/app/src/main/java/com/squad/jackbike/MapsActivity.java
+++ JackBike/app/src/main/java/com/squad/jackbike/MapsActivity.java
@@ -1,8 +1,8 @@
 package com.squad.jackbike;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -27,6 +27,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // creating request queue
         mQueue = Volley.newRequestQueue(this);
     }
 
@@ -34,8 +35,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -44,10 +44,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng montreal = new LatLng(45, -73);
-        mMap.addMarker(new MarkerOptions().position(montreal).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(montreal));
+        // Adding marker on current position using ActivityCurrentPlace
+        CurrentPlace currentPositionFinder = new CurrentPlace();
+        LatLng location = currentPositionFinder.getCurrentLocation();
+        mMap.addMarker(new MarkerOptions().position(location).title("My Location"));
+        // Moving camera on current location (will be Montreal if not found)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
         mMap.moveCamera(CameraUpdateFactory.zoomIn());
     }
 }
